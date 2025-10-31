0ad831b28ad429e6207449ac7f946921cbae92ea
deathrobloxian <65452210+deathrobloxian@users.noreply.github.com>
2025-10-31 09:41:54 -0400

    Added custom shape support to button

--- game/Menu.js
+++ game/Menu.js
@@ -1,13 +1,14 @@
 class Menu {
   menu() {
     push();
-    let but = new Button(200,200,200,100, function() {
-    if(mouseIsPressed && this.cursorDetect())
-      in_menu = false;
-  });
-    but.show()
+    let but = new Button(200, 200, 200, 100,
+      function () {
+        if (mouseIsPressed && this.cursorDetect()) in_menu = false;
+      },
+      [rect(300, 150, 200, 100)]
+    );
+    but.show();
     but.clicked();
-    blendMode(OVERLAY);
     pop();
   }
 }
