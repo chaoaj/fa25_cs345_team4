6d06e4a977d810699d9789abd3cebb05f845ca52
Joshua Dinh <dinh2jt@dukes.jmu.edu>
2025-12-01 12:04:05 -0500

    Sprint 6 fixed starship sprite sheet
    Fixed the sprite sheet by making each frame the same size and editing the Starship code to render appropiately.

--- game/Starship.js
+++ game/Starship.js
@@ -8,12 +8,12 @@ class Starship extends Platform {
     show() {
         this.x = this.start + Math.sin(frameCount / 20) * (this.end - this.start) / 2;
         push();
-        let offset = 140 * (Math.floor(frameCount / 6) % 3);
+        let offset = 370 * (Math.floor(frameCount / 6) % 3);
         if (Math.sin(frameCount / 20) - Math.sin((frameCount - 1) / 20) > 0) {
-            image(this.sprite, this.x, this.y, this.w, this.h, 113 + offset, 82, 120, 129);
+            image(this.sprite, this.x, this.y, this.w, this.h, offset, 0, 370, 370);
         } else {
             scale(-1, 1);
-            image(this.sprite, -this.x - this.w, this.y, this.w, this.h, 113 + offset, 82, 120, 129);
+            image(this.sprite, -this.x - this.w, this.y, this.w, this.h, offset, 0, 370, 370);
         }
         pop();
     }
